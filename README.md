2aa8c769c95ef5a6bca7ed90f395fa7f2050cc3d
savokiss <jaynaruto@qq.com>
2018-05-29 13:21:20 +0800

    feat: custom build

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # css-utils
-Bootstrap 4 css utils
+Bootstrap 4 css utilities
 
+## Install
 
-## docs
+```
+npm i @savo/css-utils -S
+```
+
+## Usage
+
+```javascript
+import '@savo/css-utils/dist/utilities.css'
+```
+
+## Docs
 - [Utilities](http://getbootstrap.com/docs/4.1/utilities/borders/)
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+var path = require('path')
+
+module.exports = {
+  entry: {
+    utilities: './entries/utilities.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [{
+      test: /\.(scss)$/,
+      use: [
+        MiniCssExtractPlugin.loader,
+        'css-loader',
+        'sass-loader'
+      ]
+    }]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    })
+  ]
+}
